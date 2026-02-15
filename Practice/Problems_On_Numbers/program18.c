30e8bf94658ca273bde6d1d6dd6c77433e398aee
Aashish Labade <aashishlabade2004@gmail.com>
2026-02-15 18:56:56 +0530

    Add files via upload

---
+++ Practice/Problems_On_Numbers/program18.c
@@ -0,0 +1,22 @@
+//  *   *   *   *   *
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("*\t");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
